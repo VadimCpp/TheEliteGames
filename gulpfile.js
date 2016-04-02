577f7bec21a2db6fb3545a0ad52cf0a0d66aeb22
Vadim Cpp <vadim.cpp@gmail.com>
2016-04-02 11:54:21 +0200

    Add Google-Closure to the project

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,26 @@
 
 var gulp = require('gulp');
 var clean = require('gulp-clean');
+var closureCompiler = require('gulp-closure-compiler');
+var runSequence = require('run-sequence');
+
+gulp.task('js', function() {
+    return gulp.src(['src/js/app.js', 'src/js/closure-library/closure/goog/**/*.js'])
+        .pipe(closureCompiler({
+                compilerPath: 'src/js/closure-library/closure/bin/build/compiler.jar',
+                fileName: 'app.js',
+                compilerFlags: {
+                    closure_entry_point: 'App',
+                    compilation_level: 'ADVANCED_OPTIMIZATIONS',
+                    //compilation_level: 'WHITESPACE_ONLY',
+                    only_closure_dependencies: true,
+                    warning_level: 'VERBOSE'
+                }
+            })
+        )
+        .pipe(gulp.dest('dist/js'));
+
+});
 
 gulp.task('clean', function () {
     return gulp.src('dist/**/*', {read: false})
@@ -11,10 +31,12 @@ gulp.task('clean', function () {
 });
 
 gulp.task('html', function() {
-    gulp.src('src/index.html')
+    return gulp.src('src/index.html')
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', ['clean', 'html'] );
+gulp.task('build', function(callback) {
+    runSequence('clean', 'html', 'js', callback);
+});
 
 gulp.task('default', ['build']);
--- readme.txt
+++ readme.txt
@@ -1,10 +1,11 @@
 
 Setting up working environment:
 
+
 1. Add string to the host file: "127.0.0.1 theelitegames.lc"
 
-2. Update virtual hosts and restart apache:
 
+2. Update virtual hosts and restart apache:
 <VirtualHost *:80>
     ServerName theelitegames.lc
 	DocumentRoot "~/Sites/theelitegames.lc/dist"
@@ -15,10 +16,27 @@ Setting up working environment:
 	</Directory>
 </VirtualHost>
 
-3. Install new gulp project locally:
 
+3. Install new gulp project locally:
 > npm install --save-dev gulp
 
-4. Install gulp-clean
 
+4. Install gulp-clean
 > npm install --save-dev gulp-clean
+
+
+5. Go to ./src/js/ folder and clone GoogleClosure:
+> git clone https://github.com/google/closure-library
+
+
+6. Download http://dl.google.com/closure-compiler/compiler-latest.zip and copy compiler.jar
+   to closure-library/closure/bin/build/
+
+
+7. Install gulp-closure-compiler
+> npm install --save-dev gulp-closure-compiler
+
+
+8. Install run-sequence
+> npm install --save-dev run-sequence
+
